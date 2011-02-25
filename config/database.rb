5efc0bdd862bd20578a299f2b53371aa16cf3646
Fabrice Armisen <farmisen@gmail.com>
2011-02-24 17:23:45 -0800

    throw exception on save failure

--- config/database.rb
+++ config/database.rb
@@ -1,5 +1,5 @@
 Sequel::Model.plugin(:schema)
-Sequel::Model.raise_on_save_failure = false # Do not throw exceptions on failure
+Sequel::Model.raise_on_save_failure = true # Do not throw exceptions on failure
 DB = case Padrino.env
   when :development then Sequel.connect("postgres://localhost/pothop_development", :loggers => [logger])
   when :production  then Sequel.connect("postgres://localhost/pothop_production",  :loggers => [logger])
