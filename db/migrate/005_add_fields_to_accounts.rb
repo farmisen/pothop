547dbedd5420a6c710bb17377dbf4e923e8305c9
Fabrice Armisen <farmisen@gmail.com>
2011-03-01 18:10:40 -0800

    add city id to account

---
+++ db/migrate/005_add_fields_to_accounts.rb
@@ -0,0 +1,13 @@
+class AddFieldsToAccounts < Sequel::Migration
+  def up
+    alter_table :accounts do
+      add_foreign_key :city_id, :cities
+    end
+  end
+
+  def down
+    alter_table :accounts do
+      drop_column :city_id
+    end
+  end
+end
